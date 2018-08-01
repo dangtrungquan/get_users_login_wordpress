4e267cc81c083e1028d69780360daa336c5a6552
Dang Trung Quan <dangquan919@gmail.com>
2018-08-01 10:32:49 +0700

    Cập nhật lần 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$current_user = wp_get_current_user();
+echo 'Username: ' . $current_user->user_login . '<br />';
+echo 'User email: ' . $current_user->user_email . '<br />';
+echo 'User first name: ' . $current_user->user_firstname . '<br />';
+echo 'User last name: ' . $current_user->user_lastname . '<br />';
+echo 'User display name: ' . $current_user->display_name . '<br />';
+echo 'User ID: ' . $current_user->ID . '<br />';	
+echo get_avatar( $current_user->user_email, 32 );  // 32 là kích thu?c ?nh width: 32px; height:32px;	
+?>
+
+wp_logout(); //Hàm dang xu?t chèn vào th? href
