af9244dd4a8ac7b6ad0415365decf9be827308ce
dangtrungquan <41416138+dangtrungquan@users.noreply.github.com>
2018-08-01 10:34:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-$current_user = wp_get_current_user();
-echo 'Username: ' . $current_user->user_login . '<br />';
-echo 'User email: ' . $current_user->user_email . '<br />';
-echo 'User first name: ' . $current_user->user_firstname . '<br />';
-echo 'User last name: ' . $current_user->user_lastname . '<br />';
-echo 'User display name: ' . $current_user->display_name . '<br />';
-echo 'User ID: ' . $current_user->ID . '<br />';	
-echo get_avatar( $current_user->user_email, 32 );  // 32 l‡ kÌch thu?c ?nh width: 32px; height:32px;	
+ $current_user = wp_get_current_user();
+ echo 'Username: ' . $current_user->user_login . '<br />';
+ echo 'User email: ' . $current_user->user_email . '<br />';
+ echo 'User first name: ' . $current_user->user_firstname . '<br />';
+ echo 'User last name: ' . $current_user->user_lastname . '<br />';
+ echo 'User display name: ' . $current_user->display_name . '<br />';
+ echo 'User ID: ' . $current_user->ID . '<br />';	
+ echo get_avatar( $current_user->user_email, 32 );  // 32 l√† k√≠ch thu?c ?nh width: 32px; height:32px;	
 ?>
 
-wp_logout(); //H‡m dang xu?t chËn v‡o th? href
+wp_logout(); //H√†m dang xu?t ch√®n v√†o th? href
