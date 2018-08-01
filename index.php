ba88260fdc0ed44e48ab8dde20021f15cf897b99
dangtrungquan <41416138+dangtrungquan@users.noreply.github.com>
2018-08-01 10:34:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  echo 'User last name: ' . $current_user->user_lastname . '<br />';
  echo 'User display name: ' . $current_user->display_name . '<br />';
  echo 'User ID: ' . $current_user->ID . '<br />';	
- echo get_avatar( $current_user->user_email, 32 );  // 32 là kích thu?c ?nh width: 32px; height:32px;	
+ echo get_avatar( $current_user->user_email, 32 );  // 32 là kích thước ảnh width: 32px; height:32px;	
 ?>
 
-wp_logout(); //Hàm dang xu?t chèn vào th? href
+wp_logout(); //Hàm đăng xuất chèn vào thẻ href
